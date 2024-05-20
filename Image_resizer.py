3b309553cae00d7d4e9b2cd67ea7cf0e8ab4f54a
RJannat <ramimk097@gmail.com>
2024-05-20 00:24:21 -0400

    nusing cv2 module for the project.

---
+++ Image_resizer.py
@@ -0,0 +1,11 @@
+import cv2
+
+image = cv2.imread('rjannat.jpg')
+width = int(image.shape[1] * 50 / 100)
+height = int(image.shape[1] * 50 / 100)
+
+
+
+resized_image = cv2.resize(image,(width,height))
+cv2.imwrite('resized_image.jpg', resized_image)
+print(image.shape[1])
